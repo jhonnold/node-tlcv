fdd0247d13e036c97422f30880908fde6f57905d
Jay Honnold <jayhonnold@gmail.com>
2021-09-07 18:12:59 -0700

    Use DNS to lookup address for hostname

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ type NodeTLCVConfig = {
 };
 
 export const config: NodeTLCVConfig = {
-  url: '125.237.41.141', // must remain as an IP
+  url: 'GrahamCCRL.dyndns.org',
   ports: [16001, 16002, 16053, 16063, 16065, 16066, 16083, 16084, 16091, 16092, 16093],
 };
