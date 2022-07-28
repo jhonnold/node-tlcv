7f23e48645e4653db48be96bf69765e84138f2bd
Jay Honnold <jayhonnold@gmail.com>
2022-07-27 21:21:15 -0700

    Update ports config

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ type NodeTLCVConfig = {
 
 export const config: NodeTLCVConfig = {
   url: 'GrahamCCRL.dyndns.org',
-  ports: [16001, 16002, 16053, 16063, 16065, 16066, 16083, 16084, 16091, 16092, 16093],
+  ports: [16063, 16064, 16065, 16066, 16091, 16092, 16093, 16094],
 };
