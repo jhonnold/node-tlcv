19dbc1c8e3fe67cb4b357402360a543b86530c4e
Jay Honnold <jayhonnold@gmail.com>
2023-12-16 13:18:55 -0700

    Change defaults

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ type NodeTLCVConfig = {
 
 export const config: NodeTLCVConfig = {
   url: 'GrahamCCRL.dyndns.org',
-  ports: [16061, 16062, 16063, 16065, 16068, 16091, 16092, 16093, 16094],
+  ports: [16061, 16062, 16063, 16064, 16065, 16066, 16067, 16068, 16069, 16070, 16071, 16072, 16073, 16074, 16075],
 };
